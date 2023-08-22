ffcf11eca315fc2ffe0a1944256e0246084d881f
Akshat-gour <60266468+Akshat-gour@users.noreply.github.com>
2023-08-22 15:35:03 +0530

    Update api

--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -4,7 +4,7 @@ import { LOGOUT } from '../actions/types';
 
 // Create an instance of axios
 const api = axios.create({
-  baseURL: 'http://localhost:5000/api',
+  baseURL: 'https://minitwitter-oyfe.onrender.com/api',
   headers: {
     'Content-Type': 'application/json'
   }
